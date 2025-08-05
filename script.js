5ac9a06789af935691178bb2b631ef1b320f8228
Vaishnavi  Nikam <150416104+VaishDataGeek@users.noreply.github.com>
2025-08-05 10:23:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-//your JS code here. If required.
 const inputs = document.querySelectorAll('.code');
 
+// Automatically focus on first input
 inputs[0].focus();
 
 inputs.forEach((input, index) => {
   input.addEventListener('input', (e) => {
     const value = e.target.value;
-    if (value) {
+    if (value.match(/[0-9]/)) {
       if (index < inputs.length - 1) {
         inputs[index + 1].focus();
       }
+    } else {
+      e.target.value = '';
     }
   });
 
@@ -17,6 +19,7 @@ inputs.forEach((input, index) => {
     if (e.key === 'Backspace') {
       if (input.value === '' && index > 0) {
         inputs[index - 1].focus();
+        inputs[index - 1].value = '';
       }
     }
   });
